b5a9fa531b2bfd281c860e10356aa4a632a90722
SAIF UR REHMAN <155957519+saif123-wc@users.noreply.github.com>
2024-01-25 14:58:29 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+let length=('20');
+console.log(length);
+let widgth=('30');
+console.log(widgth);
+const area=length * widgth;
+console.log(area);
+
+let name=prompt('whats your name?');
+let gret=alert('there is no friend like an old friend who has shared our morning days,no greeting like his welcome,no homage like his praise ');
+const username=('saif');
+console.log(username);
+
